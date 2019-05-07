2d9426f8cae1e0481e7e522e69b9e8b15e6a6d42
Haben Amare <haben.amare@outlook.com>
2019-05-07 23:03:27 +0300

    add test for 'dirstack' and add Travis CI config

---
+++ pkg/dirtree/dirstack_test.go
@@ -0,0 +1,95 @@
+package dirtree
+
+import "testing"
+
+func TestNewDirStack(t *testing.T) {
+	rootFileNode := &FileNode{Name: "root"}
+	dirStack := NewDirStack(rootFileNode)
+
+	if dirStack.top.value != rootFileNode ||
+		dirStack.top.next != nil ||
+		dirStack.len != 1 {
+		t.Error("DirStack was not initialized properly.")
+	}
+}
+
+func TestPush(t *testing.T) {
+	rootFileNode := &FileNode{Name: "root"}
+	dirStack := NewDirStack(rootFileNode)
+
+	aFileNode := &FileNode{Name: "Some FileNode struct"}
+	dirStack.Push(aFileNode)
+
+	if dirStack.top.value != aFileNode ||
+		dirStack.top.next.value != rootFileNode ||
+		dirStack.len != 2 {
+		t.Error("FileNode was not pushed to the DirStack properly.")
+	}
+
+	anotherFileNode := &FileNode{Name: "Another FileNode"}
+	dirStack.Push(anotherFileNode)
+
+	if dirStack.top.value != anotherFileNode ||
+		dirStack.top.next.value != aFileNode ||
+		dirStack.len != 3 {
+		t.Error("FileNode was not pushed to the DirStack properly.")
+	}
+}
+
+func TestPop(t *testing.T) {
+	rootFileNode := &FileNode{Name: "root"}
+	dirStack := NewDirStack(rootFileNode)
+
+	aFileNode := &FileNode{Name: "Some FileNode struct"}
+	dirStack.Push(aFileNode)
+
+	anotherFileNode := &FileNode{Name: "Another FileNode"}
+	dirStack.Push(anotherFileNode)
+
+	popedFileNode := dirStack.Pop()
+	if popedFileNode != anotherFileNode ||
+		dirStack.top.value != aFileNode ||
+		dirStack.len != 2 {
+		t.Error("[1] DirStack did not pop a FileNode properly.")
+	}
+
+	popedFileNode2 := dirStack.Pop()
+	if popedFileNode2 != aFileNode ||
+		dirStack.top.value != rootFileNode ||
+		dirStack.len != 1 {
+		t.Error("[2] DirStack did not pop a FileNode properly.")
+	}
+
+	popedFileNode3 := dirStack.Pop()
+	if popedFileNode3 != rootFileNode ||
+		dirStack.top.value != rootFileNode ||
+		dirStack.len != 1 {
+		t.Error("[3] DirStack did not pop a FileNode properly.")
+	}
+
+	dirStack.Pop()
+	dirStack.Pop()
+	dirStack.Pop()
+	popedFileNode4 := dirStack.Pop()
+	if popedFileNode4 != rootFileNode ||
+		dirStack.len != 1 {
+		t.Error("DirStack did not perform the Pop operation properly.")
+	}
+
+}
+
+func TestString(t *testing.T) {
+	rootFileNode := &FileNode{Name: "root"}
+	dirStack := NewDirStack(rootFileNode)
+
+	aFileNode := &FileNode{Name: "Some FileNode struct"}
+	dirStack.Push(aFileNode)
+
+	anotherFileNode := &FileNode{Name: "Another FileNode"}
+	dirStack.Push(anotherFileNode)
+
+	testString := "Length: 3\nItems [bottom -> top]: Another FileNode -> Some FileNode struct -> root"
+	if dirStack.String() != testString {
+		t.Error("DirStack string representation is incorrect.")
+	}
+}
